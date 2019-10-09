4775c0dba5307b58de2867c9199bf1c77d2bac3f
dj-hirrot <hirotoshibutani@gmail.com>
2019-10-10 04:38:50 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: 'home#index'
+
+  ActiveAdmin.routes(self)
+
+  namespace :api, { format: 'json' } do
+    namespace :v1 do
+      resources :employees, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+end
